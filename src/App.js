37c6ada72565da8b2f9eb8d43b235611212b24b2
ali_ahmed98 <umar.mukhtar4455@gmail.com>
2023-11-15 17:52:54 +0500

    used react-helmet

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import logo from './logo.svg';
-import React, { useEffect } from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
 function App() {
-
   const readDataAndSetConsentPref = (data, dt) => {
-    console.log("i am read data first", data.detail,data, dt, window,'pop',window.OneTrust, window.OneTrust?.isReady)
-    if(window.OneTrust){
-      console.log("i am read data second", data.detail)
+    console.log(
+      "i am read data first",
+      data.detail,
+      data,
+      dt,
+      window,
+      "pop",
+      window.OneTrust,
+      window.OneTrust?.isReady
+    );
+    if (window.OneTrust) {
+      console.log("i am read data second", data.detail);
     }
-
-    // if (window.utag.gdpr.getSelectedCategories().length) {
-    //   console.log("i am read data first if")
-    //   window.utag.view(window.utag.data);
-    // }
   };
 
   useEffect(() => {
-    console.log("test this", window.OneTrust, window.OneTrust?.isReady)
-    window.addEventListener('consent.onetrust', readDataAndSetConsentPref)
+    window.addEventListener("consent.onetrust", readDataAndSetConsentPref);
 
     // Cleanup the event listener when the component unmounts
     return () => {
       // Assuming you need to remove the same event listener
-      window.removeEventListener('consent.onetrust', readDataAndSetConsentPref);
+      window.removeEventListener("consent.onetrust", readDataAndSetConsentPref);
     };
-  }, []); 
+  }, []);
   return (
     <div className="App">
+      <Helmet>
+        <script
+          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+          type="text/javascript"
+          charset="UTF-8"
+          data-domain-script="64d371f7-dc2c-4262-8a2f-51c3110fec45-test"
+        ></script>
+        <script type="text/javascript">function OptanonWrapper() {}</script>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
