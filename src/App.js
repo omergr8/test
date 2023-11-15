79a9909d8bda4cc92d7b7e24aedcd55f987255c3
ali_ahmed98 <umar.mukhtar4455@gmail.com>
2023-11-15 18:37:35 +0500

    removed the dep array

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     return () => {
       window.removeEventListener("consent.onetrust", readDataAndSetConsentPref);
     };
-  }, []);
+  });
 
   return (
     <div className="App">
