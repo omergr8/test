6e61907fe9bc2972ea3640d0c5c86a373a7227c7
ali_ahmed98 <umar.mukhtar4455@gmail.com>
2023-11-15 18:04:29 +0500

    removed helmet

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import logo from "./logo.svg";
-import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
-import "./App.css";
+import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import './App.css';
 
 function App() {
+
   const readDataAndSetConsentPref = (data, dt) => {
-    console.log(
-      "i am read data first",
-      data.detail,
-      data,
-      dt,
-      window,
-      "pop",
-      window.OneTrust,
-      window.OneTrust?.isReady
-    );
-    if (window.OneTrust) {
-      console.log("i am read data second", data.detail);
+    console.log("i am read data first", data.detail,data, dt, window,'pop',window.OneTrust, window.OneTrust?.isReady)
+    if(window.OneTrust){
+      console.log("i am read data second", data.detail)
     }
+
   };
 
   useEffect(() => {
-    window.addEventListener("consent.onetrust", readDataAndSetConsentPref);
+    window.addEventListener('consent.onetrust', readDataAndSetConsentPref)
 
     // Cleanup the event listener when the component unmounts
     return () => {
       // Assuming you need to remove the same event listener
-      window.removeEventListener("consent.onetrust", readDataAndSetConsentPref);
+      window.removeEventListener('consent.onetrust', readDataAndSetConsentPref);
     };
-  }, []);
+  }, []); 
   return (
     <div className="App">
       <header className="App-header">
@@ -45,15 +37,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Helmet>
-        <script
-          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-          type="text/javascript"
-          charset="UTF-8"
-          data-domain-script="64d371f7-dc2c-4262-8a2f-51c3110fec45-test"
-        />
-        <script type="text/javascript">{`function OptanonWrapper() {}`}</script>
-      </Helmet>
     </div>
   );
 }
