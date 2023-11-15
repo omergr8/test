735da5e9df4137891eebf1a2b9c75ed3034b3e6d
ali_ahmed98 <umar.mukhtar4455@gmail.com>
2023-11-15 16:06:24 +0500

    test v1.2

--- src/App.js
+++ src/App.js
@@ -4,104 +4,68 @@ import './App.css';
 
 function App() {
 
-  // const oneTrustTagIdToTealiumTemplateIdConfig = {
-  //   "81878": "7110", // e.g.: Google Analytics
-  //   "81879": "19004", // e.g.: Adobe Analytics
-  //   "81876": "2011", // e.g.: BlueKai
-  //   "106": "13099", // e.g.: Maxymiser
-  // };
-  // End Config
-
-  const resetLoader = (u) => {
-    console.log("test reset loader",u)
-    // for (const uid in u.loader.cfg) {
-    //   if (uid !== '18') {
-    //     u.loader.cfg[uid].load = 0;
-    //   }
-    // }
-  };
-
-  const addListener = (element, event, callback) => {
-    if (element.addEventListener) {
-      element.addEventListener(event, callback, false);
-    } else if (element.attachEvent) {
-      element.attachEvent('on' + event, callback);
-    }
-  };
-
-  const getCookie = (cookie_name) => {
-    const name = cookie_name + '=';
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return '';
-  };
-
-  const readCookieAndCreateObj = (cookie_name) => {
-    let groups = '';
-    let groupArr = [];
-    let cookie = '';
-    let groupObj = {};
-    let elem = [];
-    cookie = getCookie(cookie_name);
-    console.log("test readCookieAndCreateObj", cookie_name, cookie)
-    if (cookie && cookie !== '') {
-      cookie = cookie.split('&');
-    } else {
-      return '';
-    }
-    for (let i = 0; i < cookie.length; i++) {
-      if (/groups/gi.test(cookie[i])) {
-        groups = cookie[i].split('=')[1].replace(/0_/g, '');
-      }
-    }
-    groupArr = groups.split(',');
-    for (let j = 0; j < groupArr.length; j++) {
-      elem = groupArr[j].split(':');
-      groupObj[elem[0]] = elem[1];
-    }
-    return groupObj;
-  };
-
-  const compareIds = () => {
-    const groupObj = readCookieAndCreateObj('OptanonConsent');
-    console.log("test compareids", groupObj)
-    // if (groupObj === '') {
-    //   return;
-    // }
-    // for (const oneTrustTagId in groupObj) {
-    //   if (groupObj[oneTrustTagId] === '1') {
-    //     for (const key in window.utag.loader.cfg) {
-    //       if (oneTrustTagIdToTealiumTemplateIdConfig[oneTrustTagId] == window.utag.loader.cfg[key].tid) {
-    //         window.utag.loader.cfg[key].load = 1;
-    //       }
-    //     }
-    //   }
-    // }
-  };
-
-  const readCookieAndSetConsentPref = () => {
-    resetLoader(window.utag);
-    compareIds();
-  };
-
   useEffect(() => {
-    resetLoader(window.utag);
-    compareIds();
-    addListener(window, 'consent.onetrust', readCookieAndSetConsentPref);
+    const addListener = (element, event, callback) => {
+      if (element.addEventListener) {
+        element.addEventListener(event, callback, false);
+      } else if (element.attachEvent) {
+        element.attachEvent('on' + event, callback);
+      }
+    };
+
+    const getCookie = (cookie_name) => {
+      const name = cookie_name + '=';
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return '';
+    };
+
+    const readCookieAndCreateObj = (cookie_name) => {
+      let groups = '';
+      let groupArr = [];
+      let cookie = '';
+      let groupObj = {};
+      let elem = [];
+      cookie = getCookie(cookie_name);
+      console.log("test readCookieAndCreateObj", cookie, cookie_name)
+      if (cookie && cookie !== '') {
+        cookie = cookie.split('&');
+      } else {
+        return '';
+      }
+      for (let i = 0; i < cookie.length; i++) {
+        if (/groups/gi.test(cookie[i])) {
+          groups = cookie[i].split('=')[1].replace(/0_/g, '');
+        }
+      }
+      groupArr = groups.split(',');
+      for (let j = 0; j < groupArr.length; j++) {
+        elem = groupArr[j].split(':');
+        groupObj[elem[0]] = elem[1];
+      }
+      return groupObj;
+    };
+
+    const readDataAndSetConsentPref = (data, dt) => {
+      const groupObj = readCookieAndCreateObj('OptanonConsent');
+      console.log("test readdata", groupObj)
+    };
+
+    addListener(window, 'consent.onetrust', readDataAndSetConsentPref);
 
     // Cleanup the event listener when the component unmounts
     return () => {
-      window.removeEventListener('consent.onetrust', readCookieAndSetConsentPref);
+      // Assuming you need to remove the same event listener
+      window.removeEventListener('consent.onetrust', readDataAndSetConsentPref);
     };
   }, []); 
   return (
